2011f2c7c71e1cb9c720c8b03bf154ebe36f0c59
dtp06077 <dtp06077@gmail.com>
2024-05-05 01:19:08 +0900

    SWEA 20551번 증가하는 사탕 수열

---
+++ src/Math/SWEA_20551.java
@@ -0,0 +1,41 @@
+package Math;
+
+import java.io.*;
+import java.util.*;
+
+public class SWEA_20551 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int index = 0;
+        int[] answer = new int[T];
+
+        while(index<T) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+
+            if(C<3||B<2) {
+                answer[index]=-1;
+            }
+
+            else {
+                int count = 0;
+                if(B>=C) {
+                    count+=B-(C-1);
+                    B=C-1;
+                }
+                if(A>=B) {
+                    count+=A-(B-1);
+                }
+                answer[index]=count;
+            }
+            index++;
+        }
+
+        for(int i=1;i<=T;i++) {
+            System.out.println("#"+i+" "+answer[i-1]);
+        }
+    }
+}
